258b5756fa450c26bfda602823f154aecdb5e846
Mohamed Amine Smires <mohamedamines@outlook.com>
2022-10-05 00:03:50 -0400

    Initial Commit

---
+++ FootballApp/FootballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAppApp.swift
+//  FootballApp
+//
+//  Created by Mohamed Smires on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FootballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
